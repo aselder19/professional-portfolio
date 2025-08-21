b3033a9bd8300145d185e8766a5e0d1fb1596cdd
aselder19 <andrewstantonelder@gmail.com>
2025-08-20 18:38:01 -0700

    Create BankAccount.java
    Add BankAccount class

---
+++ BankAccount.java
@@ -0,0 +1,43 @@
+// BankAccount.java
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolder;
+    private double balance;
+
+    // Constructor to create a new account
+    public BankAccount(String accountNumber, String accountHolder, double initialDeposit) {
+        this.accountNumber = accountNumber;
+        this.accountHolder = accountHolder;
+        this.balance = initialDeposit;
+    }
+
+    // Method to deposit money
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Successfully deposited $" + amount);
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+
+    // Method to withdraw money
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Successfully withdrew $" + amount);
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
+
+    // Method to check balance
+    public void checkBalance() {
+        System.out.println("Account Balance for " + accountHolder + ": $" + balance);
+    }
+
+    // Getters
+    public String getAccountNumber() { return accountNumber; }
+    public String getAccountHolder() { return accountHolder; }
+    public double getBalance() { return balance; }
+}
